4e42a794824223f8fe88f01214f0f7c973b2c2e0
Андрей Ткачук <tka4uk.andrei@yandex.ru>
2019-04-19 14:47:02 +0300

    add activity that response for showing check info

---
+++ app/src/main/java/com/example/qra/activities/ShowCheckInfoActivity.java
@@ -0,0 +1,15 @@
+package com.example.qra.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.qra.R;
+
+public class ShowCheckInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_check_info);
+    }
+}

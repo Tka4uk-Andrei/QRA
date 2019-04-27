a96544fe5a8dd919969039dcc743d0b694b8edcb
Андрей Ткачук <tka4uk.andrei@yandex.ru>
2019-04-28 01:10:24 +0300

    Add error dialog

---
+++ app/src/main/java/com/example/qra/presenter/ErrorDialog.java
@@ -0,0 +1,32 @@
+package com.example.qra.presenter;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Class for showing errors
+ */
+public class ErrorDialog extends DialogFragment {
+
+    private String str = "exception called";
+
+    public void setMsg(String msg){
+        str = msg;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+
+        return alertDialogBuilder
+                .setTitle("Exception")
+                .setMessage(str)
+                .setNeutralButton("Ok", null)
+                .create();
+    }
+}

c56324b87a252aae6bae0f643a6cc44399099167
Андрей Ткачук <tka4uk.andrei@yandex.ru>
2019-04-28 01:10:44 +0300

    improved code. Plus fixed bug when document type string was with end of line symbol

--- app/src/main/java/com/example/qra/model/qrCode/QrData.java
+++ app/src/main/java/com/example/qra/model/qrCode/QrData.java
@@ -17,43 +17,34 @@ public class QrData {
     private String buyTime;
 
     /**
-     * QrData constructor
-     *
-     * @param rawData String that recognized from QR code.
-     *                Example "t=20190124T1744&s=410.90&fn=9251440300003811&i=8947&fp=3163913062&n=1"
+     * Set time to required format
+     * @param date time in some format
+     * @return formatted date
      */
-    public QrData(String rawData) {
-        fiscalNumber = getSubString(rawData, rawData.indexOf(FISCAL_NUMBER_SEQUENCE) + FISCAL_NUMBER_SEQUENCE.length());
-        fiscalDocument = getSubString(rawData, rawData.indexOf(FISCAL_DOCUMENT_SEQUENCE) + FISCAL_DOCUMENT_SEQUENCE.length());
-        fiscalSignOfDocument = getSubString(rawData, rawData.indexOf(FISCAL_SIGN_SEQUENCE) + FISCAL_SIGN_SEQUENCE.length());
-        totalCheckSum = getSubString(rawData, rawData.indexOf(SUM_SEQUENCE) + SUM_SEQUENCE.length());
-        typeOfFiscalDocument = getSubString(rawData, rawData.indexOf(FISCAL_TYPE_SEQUENCE) + FISCAL_TYPE_SEQUENCE.length());
-        buyTime = getSubString(rawData, rawData.indexOf(TIME_SEQUENCE) + TIME_SEQUENCE.length());
-
-        totalCheckSum = changeFormatTotalCheckSum();
-        buyTime = changeFormatBuyTime();
-    }
-
-    private String changeFormatBuyTime() {
+    private String changeFormatBuyTime(String date) {
         StringBuilder sb = new StringBuilder();
-        sb.append(buyTime.substring(0, 4)).append("-").append(buyTime.substring(4, 6));
-        sb.append("-").append(buyTime.substring(6, 11)).append(":").append(buyTime.substring(11, 13));
-        if (buyTime.length() == 13) {
+        sb.append(date.substring(0, 4)).append("-").append(date.substring(4, 6));
+        sb.append("-").append(date.substring(6, 11)).append(":").append(date.substring(11, 13));
+        if (date.length() == 13) {
             return sb.toString();
         } else {
-            sb.append(":").append(buyTime.substring(13));
+            sb.append(":").append(date.substring(13));
         }
         return sb.toString();
     }
 
-
-    private String changeFormatTotalCheckSum() {
+    /**
+     * Set sum to required format
+     * @param sum sum in rubbles
+     * @return sum in kopeck
+     */
+    private String changeFormatTotalCheckSum(String sum) {
         StringBuilder sb = new StringBuilder();
         int i = 0;
-        while (totalCheckSum.charAt(i) != '.') {
+        while (sum.charAt(i) != '.') {
             i++;
         }
-        sb.append(totalCheckSum.substring(0, i)).append(totalCheckSum.substring(i + 1));
+        sb.append(sum.substring(0, i)).append(sum.substring(i + 1));
         return sb.toString();
     }
 
@@ -67,12 +58,28 @@ public class QrData {
     private String getSubString(String str, int charPos) {
         StringBuilder ans = new StringBuilder();
 
-        while (str.length() > charPos && str.charAt(charPos) != '&') {
+        while (str.length() > charPos && str.charAt(charPos) != '&' &&  str.charAt(charPos) != '\0') {
             ans.append(str.charAt(charPos));
             charPos++;
         }
 
         return ans.toString();
+    }
+
+    /**
+     * QrData constructor
+     *
+     * @param rawData String that recognized from QR code.
+     *                Example "t=20190124T1744&s=410.90&fn=9251440300003811&i=8947&fp=3163913062&n=1"
+     */
+    public QrData(String rawData) {
+        fiscalNumber = getSubString(rawData, rawData.indexOf(FISCAL_NUMBER_SEQUENCE) + FISCAL_NUMBER_SEQUENCE.length());
+        fiscalDocument = getSubString(rawData, rawData.indexOf(FISCAL_DOCUMENT_SEQUENCE) + FISCAL_DOCUMENT_SEQUENCE.length());
+        fiscalSignOfDocument = getSubString(rawData, rawData.indexOf(FISCAL_SIGN_SEQUENCE) + FISCAL_SIGN_SEQUENCE.length());
+
+        totalCheckSum = changeFormatTotalCheckSum(getSubString(rawData, rawData.indexOf(SUM_SEQUENCE) + SUM_SEQUENCE.length()));
+        typeOfFiscalDocument = getSubString(rawData, rawData.indexOf(FISCAL_TYPE_SEQUENCE) + FISCAL_TYPE_SEQUENCE.length());
+        buyTime = changeFormatBuyTime(getSubString(rawData, rawData.indexOf(TIME_SEQUENCE) + TIME_SEQUENCE.length()));
     }
 
     public QrData(String fiscalNum, String fiscalDoc, String fiscalSignOfDoc) {

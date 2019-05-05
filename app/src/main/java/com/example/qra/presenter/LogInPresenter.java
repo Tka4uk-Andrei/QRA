47a67633412f4df802324d459bed744ff4a6a9b7
Andrey Tkachuk <tka4uk.andrei@yandex.ru>
2019-05-06 00:12:32 +0200

    add login screen funtionality and support (previous same massage is bad)

--- app/src/main/java/com/example/qra/presenter/LogInPresenter.java
+++ app/src/main/java/com/example/qra/presenter/LogInPresenter.java
@@ -1,17 +1,54 @@
 package com.example.qra.presenter;
 
-import com.example.qra.presenter.interfaces.ILoginPresenter;
+import android.content.SharedPreferences;
 
+import com.example.qra.model.UserDataForFns;
+import com.example.qra.presenter.interfaces.ILoginPresenter;
+import com.example.qra.view.interfaces.ILoginView;
+
+import static android.content.Context.MODE_PRIVATE;
+
+// TODO documentation
 public class LogInPresenter implements ILoginPresenter {
 
-    public LogInPresenter(){
+    private static final String FIRST_TIME_RUN = "First time";
+    private static final String IS_FIRST_TIME = "is first";
 
+    private ILoginView loginView;
+
+    public LogInPresenter(ILoginView loginView) {
+        this.loginView = loginView;
     }
 
     @Override
-    public String singIn(String password, String login) {
+    public void onCreate() {
+
+        SharedPreferences firstTimePreference = loginView.getContext().getSharedPreferences(FIRST_TIME_RUN, MODE_PRIVATE);
+        boolean isFirstLaunch = firstTimePreference.getBoolean(IS_FIRST_TIME, true);
+
+        if (isFirstLaunch) {
+            SharedPreferences.Editor editor = firstTimePreference.edit();
+            editor.putBoolean(IS_FIRST_TIME, false);
+            editor.apply();
+        } else {
+            loginView.showLoginSucceededMessage();
+        }
+    }
+
+    @Override
+    public void singUp(String password, String login) {
         //TODO send login request
-        return ILoginPresenter.ENTER_SUCCESSFUL;
+
+        // if login succeed
+        if (password.length() != 0 && login.length() != 0) {
+            UserDataForFns.getInstance(loginView.getContext()).setPassword(password);
+            UserDataForFns.getInstance(loginView.getContext()).setPhoneNumber(login);
+
+            loginView.showLoginSucceededMessage();
+            return;
+        }
+
+        loginView.showLoginNotSucceededMessage();
     }
 
 }
--- app/src/main/java/com/example/qra/view/dialogs/LoginNotSucceededMessage.java
+++ app/src/main/java/com/example/qra/view/dialogs/LoginNotSucceededMessage.java
@@ -1,4 +1,20 @@
 package com.example.qra.view.dialogs;
 
-public class LoginNotSucceededMessage {
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+
+public class LoginNotSucceededMessage extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getActivity())
+                .setMessage("Не удалось войти. Неверный логин или пароль")
+                .setNeutralButton("Ok", null)
+                .create();
+    }
 }

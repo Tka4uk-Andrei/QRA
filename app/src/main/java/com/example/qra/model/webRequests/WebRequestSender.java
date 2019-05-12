40434f86bce0a654ecb320fec3d967740b2b0f51
Andrey Tkachuk <tka4uk.andrei@yandex.ru>
2019-05-12 19:00:27 +0300

    Improoved logInWebRequest method. Now it updates credentials

--- app/src/main/java/com/example/qra/model/webRequests/WebRequestSender.java
+++ app/src/main/java/com/example/qra/model/webRequests/WebRequestSender.java
@@ -144,7 +144,7 @@ public class WebRequestSender {
     }
 
     /**
-     * Method that log in user \\
+     * Method that log in user and updates registration data like email and name \\
      *
      * @param userData current user data \\
      * @autor : Ekaterina Novoselova
@@ -170,6 +170,12 @@ public class WebRequestSender {
                     StringBuilder sb = new StringBuilder();
                     sb.append("Error code is ").append(((Integer)responseCode[0]).toString());
                     exception[0] = new WebRequestException(responseCode[0], sb.toString());
+                } else {
+                    String msg = connection.getResponseMessage();
+                    JSONObject json = new JSONObject(msg);
+
+                    userData.setUserName(json.getString("email"));
+                    userData.setUserEmail(json.getString("name"));
                 }
             } catch (Exception e) {
                 if (exception[0] == null) {

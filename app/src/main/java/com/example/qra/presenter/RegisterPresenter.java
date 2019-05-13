6773e27dd63fd207b5c385f05eb82ce439492914
Andrey Tkachuk <tka4uk.andrei@yandex.ru>
2019-05-14 02:03:00 +0300

    add registration feature

--- app/src/main/java/com/example/qra/presenter/RegisterPresenter.java
+++ app/src/main/java/com/example/qra/presenter/RegisterPresenter.java
@@ -1,6 +1,10 @@
 package com.example.qra.presenter;
 
+import android.os.Handler;
+import android.os.Message;
+
 import com.example.qra.model.UserDataForFns;
+import com.example.qra.model.webRequests.requests.RegistrationWebRequest;
 import com.example.qra.presenter.interfaces.IRegisterPresenter;
 import com.example.qra.presenter.login.LogInPresenter;
 import com.example.qra.view.LogInActivity;
@@ -18,36 +22,61 @@ public class RegisterPresenter extends AndroidPresenter implements IRegisterPres
     @Override
     public void registerUser(String name, String email, String phone) {
         // update registration data
-        UserDataForFns.getInstance(getView().getContext()).setPhoneNumber();
-        UserDataForFns.getInstance(getView().getContext()).setUserEmail();
-        UserDataForFns.getInstance(getView().getContext()).setUserName();
-        UserDataForFns.getInstance(getView().getContext()).apply();
+        UserDataForFns.getInstance(getView().getContext()).setPhoneNumber(phone);
+        UserDataForFns.getInstance(getView().getContext()).setUserEmail(email);
+        UserDataForFns.getInstance(getView().getContext()).setUserName(name);
+        UserDataForFns.getInstance(getView().getContext()).apply(getView().getContext());
 
-        // TODO request
+        // send request with fail handler
+        (new Thread(new RegistrationWebRequest(
+                UserDataForFns.getInstance(getView().getContext()),
+                new RegisterNotSucceededHandler(this),
+                new RegisterSucceededHandler(this)))).start();
     }
 
-    private void registrationSucceeded(){
+    private void registrationSucceeded() {
+        // start login activity with entered phone
+        putStingExtra(LogInPresenter.STARTER_ACTIVITY, RegisterPresenter.class.getName());
+        putStingExtra(LogInPresenter.PHONE_EXTRA, UserDataForFns.getInstance(getView().getContext()).getPhoneNumber());
+        startActivity(LogInActivity.class, true);
+    }
 
-
-        if (name.length() != 0 && email.length() != 0
-                && phone.length() != 0) {
-
-            // save data in cache
-            UserDataForFns.getInstance(registerView.getContext()).setPhoneNumber(phone);
-            UserDataForFns.getInstance(null).setUserEmail(email);
-            UserDataForFns.getInstance(null).setUserName(name);
-            UserDataForFns.getInstance(null).apply(registerView.getContext());
-
-            // start login activity
-            putStingExtra(LogInPresenter.STARTER_ACTIVITY, RegisterPresenter.class.getName());
-            putStingExtra(LogInPresenter.PHONE_EXTRA, phone);
-            startActivity(LogInActivity.class, true);
-        }
+    private void registrationFailed(){
+        // todo reaction
     }
 
     @Override
     public void onCreate() {
         // nothing to do
+    }
 
+
+    private static class RegisterSucceededHandler extends Handler {
+
+        private RegisterPresenter registerPresenter;
+
+        RegisterSucceededHandler(RegisterPresenter registerPresenter) {
+            this.registerPresenter = registerPresenter;
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            registerPresenter.registrationSucceeded();
+        }
+    }
+
+
+    private static class RegisterNotSucceededHandler extends Handler {
+
+        private RegisterPresenter registerPresenter;
+
+        RegisterNotSucceededHandler(RegisterPresenter registerPresenter) {
+            this.registerPresenter = registerPresenter;
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            registerPresenter.registrationFailed();
+        }
     }
 }
